2234ccd7e4af0d8286d1b0cdc3a4b534e3d108f3
АкылбековА <akylbekov_alen@mail.ru>
2018-10-29 20:15:41 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+
+    class Program
+    {
+
+        public interface Callback
+        {
+            void Execute();
+        }
+
+        class CallbackRel : Callback
+        {
+            public void Execute()
+            {
+                Console.WriteLine("sth");
+            }
+        }
+        public class Executor
+        {
+            private Callback cb;
+            public void SetCallback(Callback cb)
+            {
+                this.cb = cb;
+            }
+            public void ExecuteAction()
+            {
+                cb.Execute();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Executor h = new Executor();
+            Callback a = new CallbackRel();
+
+            h.SetCallback(a);
+            h.ExecuteAction();
+        }
+    }
+}
